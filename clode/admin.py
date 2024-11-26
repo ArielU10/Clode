c662c5ef28bb148d537e891858c49651c17d5c08
Adrian Acuña <adriansamacu71@gmail.com>
2024-11-26 15:46:54 -0500

    Avances modelos

--- clode/admin.py
+++ clode/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(USERS)
+
+admin.site.register(USER_PREFERENCES)
+
+admin.site.register(GARMENTS)
+
+admin.site.register(GARMENT_TAGS)
+
+admin.site.register(EXCHANGE)
+
+admin.site.register(SCORES)
